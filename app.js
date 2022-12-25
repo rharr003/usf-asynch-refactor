92ffcebecb8d08ab6ef92e0b1f3db028272a3808
Scott Harrington <robertscottharrington@gmail.com>
2022-12-25 11:12:30 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 body = document.querySelector("body");
-// function populateData(data) {
-//   trivia = document.createElement("p");
-//   trivia.innerText = data;
-//   body.insertAdjacentElement("beforeend", trivia);
-// }
+function populateData(data) {
+  trivia = document.createElement("p");
+  trivia.innerText = data;
+  body.insertAdjacentElement("beforeend", trivia);
+}
+
+async function fetchTrivia(start, stop) {
+  const response = await fetch(`http://numbersapi.com/${start}..${stop}?json`)
+    .then((response) => response.json())
+    .then((data) => data);
+
+  for (let item of Object.entries(response)) {
+    populateData(item[1]);
+  }
+}
+
+fetchTrivia(20, 25);
 
 // fetch("http://numbersapi.com/7..13?json")
 //   .then((response) => response.json())
@@ -25,24 +37,59 @@ body = document.querySelector("body");
 //getFacts(5);
 const drawButton = document.querySelector("#draw-btn");
 
-let deckID = "";
+// let deckID = "";
 
-fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-  .then((response) => response.json())
-  .then((data) => (deckID = data.deck_id));
-function drawCard() {
-  fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
-    .then((response) => response.json())
-    .then((data) => {
-      data.cards.forEach((card) => {
-        html = `<div class="card" style="transform: rotate(${
-          Math.random() * 45
-        }deg)">
-                    <img src=${card.image} />
-                  </div>`;
-        body.insertAdjacentHTML("beforeend", html);
-      });
-    });
+// fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+//   .then((response) => response.json())
+//   .then((data) => (deckID = data.deck_id));
+// function drawCard() {
+//   fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       data.cards.forEach((card) => {
+//         html = `<div class="card" style="transform: rotate(${
+//           Math.random() * 45
+//         }deg)">
+//                     <img src=${card.image} />
+//                   </div>`;
+//         body.insertAdjacentHTML("beforeend", html);
+//       });
+//     });
+// }
+
+class deck {
+  constructor() {
+    this.init();
+  }
+  async init() {
+    let response = await fetch(
+      "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+    )
+      .then((response) => response.json())
+      .then((data) => data.deck_id);
+    this.deckID = response;
+  }
+
+  populateHTML = function (card) {
+    let html = `<div class="card" style="transform: rotate(${
+      Math.random() * 45
+    }deg)">
+                <img src=${card.image} />
+              </div>`;
+    body.insertAdjacentHTML("beforeend", html);
+  };
+
+  async drawCard() {
+    let response = await fetch(
+      `https://deckofcardsapi.com/api/deck/${this.deckID}/draw/?count=1`
+    )
+      .then((response) => response.json())
+      .then((data) => data);
+    console.log(response);
+    this.populateHTML(response.cards[0]);
+  }
 }
 
-drawButton.addEventListener("click", drawCard);
+let d = new deck();
+
+drawButton.addEventListener("click", d.drawCard.bind(d));
